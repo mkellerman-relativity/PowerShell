0efcfd4133c9c56662ae9c02e00ba88856aa6736
Marc R Kellerman <103457964+mkellerman-relativity@users.noreply.github.com>
2022-09-28 12:52:30 -0700

    Update Enable-OpenSSH.ps1

--- Enable-OpenSSH/Enable-OpenSSH.ps1
+++ Enable-OpenSSH/Enable-OpenSSH.ps1
@@ -1,10 +1,26 @@
-Add-WindowsCapability -Online -Name OpenSSH.Server
-Add-WindowsCapability -Online -Name OpenSSH.Client
+Get-WindowsCapability -Online | Where-Object Name -like 'OpenSSH*' | Add-WindowsCapability -Online
 
-netsh advfirewall firewall add rule name="SSH" dir=in localport=22 protocol=TCP action=allow
+# Configuring the default shell for OpenSSH in Windows
+New-ItemProperty -Path "HKLM:\SOFTWARE\OpenSSH" -Name DefaultShell -Value "C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe" -PropertyType String -Force
 
+# Configuring Authentication Method
 "PasswordAuthentication yes" |Add-Content $env:ProgramData\ssh\sshd_config
-Set-Service sshd -StartupType Automatic
-Set-Service ssh-agent -StartupType Automatic
 
-Restart-Service sshd,ssh-agent
+# Start the sshd service
+Start-Service sshd
+
+# OPTIONAL but recommended:
+Set-Service -Name sshd -StartupType 'Automatic'
+
+# Confirm the Firewall rule is configured. It should be created automatically by setup. Run the following to verify
+if (!(Get-NetFirewallRule -Name "OpenSSH-Server-In-TCP" -ErrorAction SilentlyContinue | Select-Object Name, Enabled)) {
+    Write-Output "Firewall Rule 'OpenSSH-Server-In-TCP' does not exist, creating it..."
+    New-NetFirewallRule -Name 'OpenSSH-Server-In-TCP' -DisplayName 'OpenSSH Server (sshd)' -Enabled True -Direction Inbound -Protocol TCP -Action Allow -LocalPort 22
+} else {
+    Write-Output "Firewall rule 'OpenSSH-Server-In-TCP' has been created and exists."
+}
+
+# Set-Service sshd -StartupType Automatic
+# Set-Service ssh-agent -StartupType Automatic
+
+# Restart-Service sshd,ssh-agent
